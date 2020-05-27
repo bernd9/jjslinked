67b9f586d8e55a4edcc09ea642d9f28b68ac2ff9
bernd9 <bernd@peltzer-bartels.de>
2020-05-27 21:17:33 +0200

    cleaned up

--- JJS-Linked-Annotations/src/main/java/com/jjslink/annotations/Client.java
+++
@@ -1,9 +0,0 @@
-package com.jjslink.annotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface Client {
-    String clientClass() default "";
-}
