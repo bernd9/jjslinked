2445c3d18e80db3e14b3acd0e4b19b3d196fa8dc
bernd9 <bernd@peltzer-bartels.de>
2020-06-30 00:39:30 +0200

    stored elements incomplete

--- ejc/src/main/java/com/ejc/processor/StoredTypeElements.java
+++ ejc/src/main/java/com/ejc/processor/StoredTypeElements.java
@@ -1,4 +1,18 @@
 package com.ejc.processor;
 
-public class StoredTypeElements {
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+public class StoredTypeElements { // TODO generic superclass
+
+    void add(TypeElement e) {
+
+    }
+
+    // TODO Ein Prozessor der nur in die Dateien schreibt, damit in der Kundenimplementierung alles in einem Kontext ist ?
+    // Oder besser doch 2 Kontexte ? Vermutlich ja
+    Set<StoredTypeElement> getStored() {
+        return null;
+    }
+
 }
