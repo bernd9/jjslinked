670e98f399b74666c3f6860cca91215336ee3c22
bernd9 <bernd@peltzer-bartels.de>
2020-11-15 21:40:58 +0100

    test-api-project created

---
+++ ejc-test/src/test/java/com/ejc/junit5/SampleTest.java
@@ -0,0 +1,14 @@
+package com.ejc.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(EjcUnitTestExtension.class)
+public class SampleTest {
+
+    @Test
+    void test() {
+
+    }
+}
