02bf8b015fedd01c7142356d00ed5ab7d9fa2e03
bernd9 <bernd@peltzer-bartels.de>
2020-11-21 12:09:23 +0100

    ParameterUtils

---
+++ ejc/src/main/java/com/ejc/util/ParameterUtils.java
@@ -0,0 +1,24 @@
+package com.ejc.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Optional;
+
+@UtilityClass
+public class ParameterUtils {
+    public static Optional<Class<?>> getGenericCollectionType(Parameter parameter) {
+        if (!Collection.class.isAssignableFrom(parameter.getType())) {
+            throw new IllegalArgumentException("not a collection parameter: " + parameter);
+        }
+        Type genericType = parameter.getParameterizedType();
+        if (genericType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) parameter.getParameterizedType();
+            return Optional.of((Class<?>) parameterizedType.getActualTypeArguments()[0]);
+        }
+        return Optional.empty();
+    }
+}
